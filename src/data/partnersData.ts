8f6ae10188b732a65603cf2e862e34834cba3274
Rishitshivam <164783543+Rishitshivam@users.noreply.github.com>
2025-11-30 01:48:07 +0530

    Refactoring data for partners component

---
+++ src/data/partnersData.ts
@@ -0,0 +1,88 @@
+export interface Partner {
+  title: string;
+  imageUrl: string;
+  link: string;
+}
+
+export const partners: Partner[] = [
+  {
+    title: "Partner 1",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/1",
+  },
+  {
+    title: "Partner 2",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/2",
+  },
+  {
+    title: "Partner 3",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/3",
+  },
+  {
+    title: "Partner 4",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/4",
+  },
+  {
+    title: "Partner 5",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/5",
+  },
+  {
+    title: "Partner 6",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/6",
+  },
+  {
+    title: "Partner 1",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/1",
+  },
+  {
+    title: "Partner 2",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/2",
+  },
+  {
+    title: "Partner 3",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/3",
+  },
+  {
+    title: "Partner 3",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/3",
+  },
+  {
+    title: "Partner 3",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/3",
+  },
+  {
+    title: "Partner 3",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/3",
+  },
+  {
+    title: "Partner 3",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/3",
+  },
+  {
+    title: "Partner 3",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/3",
+  },
+  {
+    title: "Partner 3",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/3",
+  },
+  {
+    title: "Partner 3",
+    imageUrl: "/github-icon.svg",
+    link: "/partners/3",
+  },
+];
