e56b779ac4834d55fb07f36d65ef5ba80fa3a4fc
theg1239 <thegreatestdropper@gmail.com>
2025-12-24 01:19:28 +0400

    feat: add assetsPrefix configuration using PUBLIC_CDN_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 
+const cdnUrl = process.env.PUBLIC_CDN_URL?.replace(/\/$/, "");
+
 export default defineConfig({
     output: 'static',
     adapter: vercel(),
     build: {
+        assetsPrefix: cdnUrl,
     },
     vite: {
         plugins: [tailwindcss()],
